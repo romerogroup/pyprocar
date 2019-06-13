fb57d07cd86495105a1e992338e8293ea5a23ade
mailhexu <mailhexu@gmail.com>
2019-06-13 15:37:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from distutils.core import setup, Extension
-import pyprocar 
 
 setup(
     name='pyprocar',
-    version = pyprocar.__version__,
+    version = "3.8.4",
     author='Francisco Munoz,Aldo Romero,Sobhit Singh,Uthpala Herath,Pedram Tavadze,Eric Bousquet, Xu He',
     author_email='fvmunoz@gmail.com,alromero@mail.wvu.edu,smsingh@mix.wvu.edu,ukh0001@mix.wvu.edu,petavazohi@mix.wvu.edu,eric.bousquet@uliege.be,mailhexu@gmail.com',
     url='https://github.com/romerogroup/pyprocar',
