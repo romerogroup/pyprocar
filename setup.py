71198d3d2ec4c1eee6e55aa4b237814d0cdd6cca
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-01-06 16:21:47 -0500

    Merged commits from Nicholas

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup, Extension
 
 setup(
     name='pyprocar',
-    version = "4.0.2",
+    version = "4.0.3",
     author='Francisco Munoz,Aldo Romero,Sobhit Singh,Uthpala Herath,Pedram Tavadze,Eric Bousquet, Xu He',
     author_email='fvmunoz@gmail.com,alromero@mail.wvu.edu,smsingh@mix.wvu.edu,ukh0001@mix.wvu.edu,petavazohi@mix.wvu.edu,eric.bousquet@uliege.be,mailhexu@gmail.com',
     url='https://github.com/romerogroup/pyprocar',
-    download_url='https://github.com/romerogroup/pyprocar/archive/4.0.2.tar.gz',
+    download_url='https://github.com/romerogroup/pyprocar/archive/4.0.3.tar.gz',
     packages=['pyprocar', 
               'pyprocar.utilsprocar',
               'pyprocar.procarparser',
