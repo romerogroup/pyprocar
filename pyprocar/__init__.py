a3a5e7758e3f0b115baf0593fc473251beb114cc
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-03-05 17:52:53 -0500

    Fixed Abinit PROCAR formatting issues in PyProcar cat function.

--- pyprocar/__init__.py
+++ pyprocar/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Francisco Munoz,Aldo Romero,Sobhit Singh,Uthpala Herath,Pedram Tavadze,Eric Bousquet,Xu He"
 __copyright__ = "Copyright 2020"
-__version__ = "5.1.2"
+__version__ = "5.1.3"
 __email__ = (
     "fvmunoz@gmail.com/alromero@mail.wvu.edu/ukh0001@mix.wvu.edu/petavazohi@mix.wvu.edu"
 )
