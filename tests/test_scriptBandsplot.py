d13ffd6e21241f8cfc79d53fc45f962fb27637c4
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-06-02 00:08:24 -0400

    Updated tests

--- tests/test_scriptBandsplot.py
+++ tests/test_scriptBandsplot.py
@@ -1,14 +1,14 @@
 import pyprocar
 
 pyprocar.bandsplot(
-    file = '../examples/SrVO3/nospin/PROCAR3',
-    mode = 'parametric',
-    elimit = [-6,6],
-    orbitals = [4,5,6,7,8],
-    vmin = 0,
-    vmax = 1, 
-    #code = 'elk',
-    kpointsfile = '../examples/SrVO3/nospin/KPOINTS3',
-    outcar = '../examples/SrVO3/nospin/OUTCAR3',
-    savefig = '../tests/test.pdf')
-    
+    procarfile="../examples/SrVO3/nospin/PROCAR3",
+    mode="parametric",
+    elimit=[-6, 6],
+    orbitals=[4, 5, 6, 7, 8],
+    vmin=0,
+    vmax=1,
+    # code = 'elk',
+    kpointsfile="../examples/SrVO3/nospin/KPOINTS3",
+    outcar="../examples/SrVO3/nospin/OUTCAR3",
+    savefig="../tests/test.pdf",
+)
