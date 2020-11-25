2bd3ed9d9086d6ad964bec2ab97746639266d4db
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-11-25 05:10:47 -0500

    Updated setup.py to be more pythonic and efficient

---
+++ pyprocar/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM PYPROCAR SETUP.PY.
+name = 'PyProcar'
+version = '5.5.8'
+description = 'A Python library for electronic structure pre/post-processing. '
+url = 'https://github.com/romerogroup/pyprocar'
+author = 'Francisco Munoz, Aldo Romero, Sobhit Singh, Uthpala Herath, Pedram Tavadze, Eric Bousquet, Xu He'
+email = 'fvmunoz@gmail.com,alromero@mail.wvu.edu,smsingh@mix.wvu.edu,ukh0001@mix.wvu.edu,petavazohi@mix.wvu.edu,eric.bousquet@uliege.be,mailhexu@gmail.com'
+status = 'development'
+copyright = 'Copyright 2020'
+date = 'Nov 24th, 2020'
