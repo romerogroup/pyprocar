ca4cc37a5a06fae8a6eb4090e925b996da1101dc
Uthpala Herath <ukh0001@mix.wvu.edu>
2021-03-08 23:56:49 -0500

    updated abinit stuff

--- pyprocar/io/abinit.py
+++ pyprocar/io/abinit.py
@@ -1,11 +1,13 @@
 from re import findall
 from numpy import array
-from ..core import Structure, DensityOfStates, ElectronicBandStructure
+from ..core import Structure, DensityOfStates, ElectronicBandStructure, KPath
 from .vasp import Procar
 import numpy as np
+import glob
+import collections
 
 
-class AbinitParser(ElectronicBandStructure):
+class AbinitParser(collections.abc.Mapping):
     """
     This class contains methods to parse the fermi energy and reciprocal
     lattice vectors from the Abinit output file.
@@ -14,7 +16,7 @@ class AbinitParser(ElectronicBandStructure):
     the methods in vasp.py will be used to parse it.
     """
 
-    def __init__(self, abinit_output=None):
+    def __init__(self, inFiles=None, abinit_output=None):
 
         self.abinit_output = abinit_output
         self.fermi = None
@@ -23,6 +25,25 @@ class AbinitParser(ElectronicBandStructure):
 
         self._readFermi()
         self._readRecLattice()
+
+        # Preparing files for merging
+        # reading in all PROCAR_* files and putting it into a list if not provided.
+        if inFiles is None:
+            inFiles = sorted(glob.glob("PROCAR_*"))
+        else:
+            inFiles = inFiles
+
+        if isinstance(inFiles, list):
+            self._mergeparallel(
+                inputfiles=inFiles,
+                outputfile="PROCAR",
+                nspin=self.nspin,
+                abinit_output=self.abinit_output,
+            )
+        else:
+            pass
+
+        self._readAbinitProcar()
 
         return
 
@@ -58,20 +79,18 @@ class AbinitParser(ElectronicBandStructure):
             dtype=float,
         )
 
-
-    def _mergeparallel(inputfiles=None, outputfile=None, nspin=1, abinit_output=None):
+    def _mergeparallel(
+        self, inputfiles=None, outputfile=None, nspin=1, abinit_output=None
+    ):
         """This merges Procar files seperated between k-point ranges.
         Happens with parallel Abinit runs.
         """
         print("Merging parallel files...")
         filenames = sorted(inputfiles)
+        print(filenames)
 
         # creating an instance of the AbinitParser class
-        if abinit_output:
-            abinitparserobject = AbinitParser(abinit_output=abinit_output)
-            nspin = int(abinitparserobject.nspin)
-        else:
-            nspin = int(nspin)
+        nspin = int(nspin)
 
         if nspin != 2:
             with open(outputfile, "w") as outfile:
@@ -83,37 +102,36 @@ class AbinitParser(ElectronicBandStructure):
         elif nspin == 2:
             # for spin polarized calculations the spin down segments are saved in the
             # second half of the PROCAR's but in reverse k-point order. So we have to
-        # fix the order and merge the second half of the PROCAR's.
-        spinup_list = filenames[: int(len(filenames) / 2)]
-        spindown_list = filenames[int(len(filenames) / 2) :]
+            # fix the order and merge the second half of the PROCAR's.
+            spinup_list = filenames[: int(len(filenames) / 2)]
+            spindown_list = filenames[int(len(filenames) / 2) :]
 
-        # reading the second line of the header to set as the separating line
-        # in the colinear spin PROCAR.
-        fp = open(spinup_list[0], "r")
-        header1 = fp.readline()
-        header2 = fp.readline()
-        fp.close()
+            # reading the second line of the header to set as the separating line
+            # in the colinear spin PROCAR.
+            fp = open(spinup_list[0], "r")
+            header1 = fp.readline()
+            header2 = fp.readline()
+            fp.close()
 
-        # second half of PROCAR files in reverse order.
-        spindown_list.reverse()
+            # second half of PROCAR files in reverse order.
+            spindown_list.reverse()
 
-        # Writing new PROCAR with first spin up, header2 and then
-        # spin down (reversed).
-        with open(outputfile, "w") as outfile:
-            for spinupfile in spinup_list:
-                with open(spinupfile) as infile:
-                    for line in infile:
-                        outfile.write(line)
-            outfile.write("\n")
-            outfile.write(header2)
-            outfile.write("\n")
-            for spindownfile in spindown_list:
-                with open(spindownfile) as infile:
-                    for line in infile:
-                        outfile.write(line)
+            # Writing new PROCAR with first spin up, header2 and then
+            # spin down (reversed).
+            with open(outputfile, "w") as outfile:
+                for spinupfile in spinup_list:
+                    with open(spinupfile) as infile:
+                        for line in infile:
+                            outfile.write(line)
+                outfile.write("\n")
+                outfile.write(header2)
+                outfile.write("\n")
+                for spindownfile in spindown_list:
+                    with open(spindownfile) as infile:
+                        for line in infile:
+                            outfile.write(line)
 
-
-    def _fixformat(inputfile=None, outputfile=None):
+    def _fixformat(self, inputfile=None, outputfile=None):
 
         """Fixes the formatting of Abinit's Procar
         when the tot projection is not summed and spin directions
@@ -261,10 +279,62 @@ class AbinitParser(ElectronicBandStructure):
         First this PROCAR will need to be fixed for
         parallelization and formatting."""
 
-        abinitprocarobject = Procar(
+        self.abinitprocarobject = Procar(
             filename="PROCAR",
             structure=None,
-            reciprocal_lattice=None,
-            permissive=False,
+            reciprocal_lattice=self.reclat,
+            kpath=None,
+            efermi=self.fermi,
             interpolation_factor=1,
         )
+
+    def __contains__(self, x):
+        return x in self.variables
+
+    def __getitem__(self, x):
+        return self.variables.__getitem__(x)
+
+    def __iter__(self):
+        return self.variables.__iter__()
+
+    def __len__(self):
+        return self.variables.__len__()
+
+
+class AbinitKpoints(collections.abc.Mapping):
+    def __init__(self, filename="abinit.out", has_time_reversal=True):
+        self.variables = {}
+        self.filename = filename
+        self.file_str = None
+        self.metadata = None
+        self.mode = None
+        self.kgrid = None
+        self.kshift = None
+        self.ngrids = None
+        self.special_kpoints = None
+        self.knames = None
+        self.cartesian = False
+        self.automatic = False
+        # self._parse_kpoints()
+        # self.kpath = KPath(
+        #     knames=self.knames,
+        #     special_kpoints=self.special_kpoints,
+        #     ngrids=self.ngrids,
+        #     has_time_reversal=has_time_reversal,
+        # )
+
+    # def _parse_kpoints(self):
+    #     self.kgrid
+    #     self.
+
+    def __contains__(self, x):
+        return x in self.variables
+
+    def __getitem__(self, x):
+        return self.variables.__getitem__(x)
+
+    def __iter__(self):
+        return self.variables.__iter__()
+
+    def __len__(self):
+        return self.variables.__len__()
