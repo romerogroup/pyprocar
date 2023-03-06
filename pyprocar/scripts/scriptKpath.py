d6dbfb67e26efc7006d9188f847bb84edf07043e
Will Laderer <94595045+wladerer@users.noreply.github.com>
2023-03-06 14:56:58 -0500

    Re: Issue #94
    Updated np.float to np.float64 and np.int_

--- pyprocar/scripts/scriptKpath.py
+++ pyprocar/scripts/scriptKpath.py
@@ -54,20 +54,20 @@ def kpath(
 
     for i in range(len(cell_matrix)):
         cell_matrix0 = np.array(cell_matrix[i].split())
-        cell[i, :] = (cell_matrix0.astype(np.float)) * np.array(
-            POSCAR[1].split()).astype(np.float)
+        cell[i, :] = (cell_matrix0.astype(np.float64)) * np.array(
+            POSCAR[1].split()).astype(np.float64)
 
     # positions
     # POSCAR index changed by Nicholas Pike from 5 -> 6 and from 7 -> 8
     # Previously, POSCAR[5] referenced the atom names i.e. Na Cl and not the
     # atom numbers
-    atoms = np.array(POSCAR[6].split()).astype(np.int)
+    atoms = np.array(POSCAR[6].split()).astype(np.int_)
     positions_matrix = POSCAR[8:8 + sum(atoms)]
     positions = np.zeros(shape=(np.sum(atoms), 3))
 
-    for j in range(len(positions_matrix)):
+    for j in range(1, len(positions_matrix)):
         positions_matrix0 = np.array(positions_matrix[j].split())[0:3]
-        positions[j, :] = positions_matrix0.astype(np.float)
+        positions[j, :] = positions_matrix0.astype(np.float64)
 
     # numbers
     numbers = np.zeros(sum(atoms))
