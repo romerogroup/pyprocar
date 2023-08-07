77ac236e642b50e7f1f4c608282543b591d5b41a
lllangWV <lllangWV@gmail.com>
2023-08-07 02:51:53 -0400

    Minor version change

--- pyprocar/version.py
+++ pyprocar/version.py
@@ -1,6 +1,6 @@
 # THIS FILE IS GENERATED FROM PYPROCAR SETUP.PY.
 name = 'PyProcar'
-version = '6.0.0'
+version = '6.1.0'
 description = 'A Python library for electronic structure pre/post-processing. '
 author = 'Francisco Muñoz, Aldo Romero, Sobhit Singh, Uthpala Herath, Pedram Tavadze, Eric Bousquet, Xu He, Reese Boucher, Logan Lang, Freddy Farah'
 email = 'fvmunoz@gmail.com, alromero@mail.wvu.edu, smsingh@mix.wvu.edu, ukh0001@mix.wvu.edu, petavazohi@mix.wvu.edu, eric.bousquet@uliege.be, mailhexu@gmail.com'
