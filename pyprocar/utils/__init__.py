3f555656d2e43b899c21225226542580e02a4705
lllangWV <lllangWV@gmail.com>
2024-06-26 02:58:04 -0400

    Updated logger and moved directory variables to config

--- pyprocar/utils/__init__.py
+++ pyprocar/utils/__init__.py
@@ -1,51 +1,21 @@
 # -*- coding: utf-8 -*-
 from typing import Union
 
-import os
-import logging.config
-from pathlib import Path
-import yaml
-
-from .utilsprocar import UtilsProcar
-from .procarfilefilter import ProcarFileFilter
-from .splash import welcome
-from . import mathematics
-from . import defaults
-from . import elements 
-from . import sorting
-from . import strings
-from .config import ConfigManager
-
-# Other Constants
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[2]  # pyprocar
-LOGGING_NAME = 'pyprocar'
-VERBOSE = str(os.getenv('PYPROCAR_VERBOSE', True)).lower() == 'true'  # global verbose mode
 
 
-def set_logging(name=LOGGING_NAME, verbose=True):
-    """Sets up logging for the given name."""
-    rank = int(os.getenv('RANK', -1))  # rank in world for Multi-GPU trainings
-    level = logging.INFO if verbose and rank in {-1, 0} else logging.ERROR
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            name: {
-                'format': '%(message)s'}},
-        'handlers': {
-            name: {
-                'class': 'logging.StreamHandler',
-                'formatter': name,
-                'level': level}},
-        'loggers': {
-            name: {
-                'level': level,
-                'handlers': [name],
-                'propagate': False}}})
-    
+from pyprocar.utils.utilsprocar import UtilsProcar
+from pyprocar.utils.procarfilefilter import ProcarFileFilter
+from pyprocar.utils.splash import welcome
+from pyprocar.utils import mathematics
+from pyprocar.utils import defaults
+from pyprocar.utils import elements 
+from pyprocar.utils import sorting
+from pyprocar.utils import strings
 
-# Set logger
-set_logging(LOGGING_NAME, verbose=VERBOSE)  # run before defining LOGGER
-LOGGER = logging.getLogger(LOGGING_NAME)  # define globally (used in train.py, val.py, detect.py, etc.)
+# Loading configuration settings
+from pyprocar.utils.config import ConfigManager
+from pyprocar.utils.config import LOG_DIR,DATA_DIR,ROOT,PKG_DIR,CONFIG
+from pyprocar.utils.log_config import setup_logging
 
+# Initialize logger
+LOGGER = setup_logging(log_dir=LOG_DIR,apply_filter=CONFIG['APPLY_LOG_FILTER'], log_level=CONFIG['log_level'])
--- pyprocar/utils/config.py
+++ pyprocar/utils/config.py
@@ -1,3 +1,9 @@
+
+
+import os
+
+from pathlib import Path
+import numpy as np
 import yaml
 
 class ConfigManager:
@@ -17,3 +23,15 @@ class ConfigManager:
     def get_config(self) -> dict:
         """Retrieve the current configuration."""
         return self.config
+    
+# Important directory paths
+FILE = Path(__file__).resolve()
+PKG_DIR = str(FILE.parents[1])  # pyprocar
+ROOT = str(FILE.parents[2])  # PyProcar
+LOG_DIR=os.path.join(ROOT,'logs')
+DATA_DIR=os.path.join(ROOT,'data')
+CONFIG_FILE=os.path.join(PKG_DIR,'cfg','config.yml')
+
+# Load config from yaml file
+with open(CONFIG_FILE, 'r') as f:
+    CONFIG = yaml.safe_load(f)
