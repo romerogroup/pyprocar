d4b95b9e9c652d274c08e8a62cd390fc6cbfa06e
GitHub Action <action@github.com>
2025-03-01 17:50:59 +0000

    Update _version.py and CHANGELOG.md due to new release

--- pyprocar/_version.py
+++ pyprocar/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '6.3.2'
-__version_tuple__ = version_tuple = (6, 3, 2)
+__version__ = version = 'v6.3.3'
+__version_tuple__ = version_tuple = (6, 3, 3)
