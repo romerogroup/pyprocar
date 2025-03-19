5de0facc9c1a1db1226ebc0b931dc92d2216f216
lllangWV <lllangWV@gmail.com>
2025-03-19 18:08:01 -0400

    Refactor parser.py for improved readability and organization
    - Consolidated import statements for better clarity.
    - Cleaned up whitespace and formatting throughout the file.
    - Enhanced the structure of the Parser class methods for better maintainability.
    - Improved logging and error handling in various parsing methods.
    - Streamlined the initialization of parser objects for different file types.

--- pyprocar/io/parser.py
+++ pyprocar/io/parser.py
@@ -1,29 +1,28 @@
+import logging
 import os
 
 import numpy as np
-import logging
 
-from ..core import ElectronicBandStructure
-from ..core import DensityOfStates
-from ..core import Structure
+from ..core import DensityOfStates, ElectronicBandStructure, Structure
 from ..utils import UtilsProcar
-from . import vasp, qe, abinit, lobster, siesta, frmsf, bxsf, elk, dftbplus
-
+from . import abinit, bxsf, dftbplus, elk, frmsf, lobster, qe, siesta, vasp
 
 logger = logging.getLogger(__name__)
 
+
 class Parser:
     """
-    The parser class will be the main object to be used through out the code. 
+    The parser class will be the main object to be used through out the code.
     This class will handle getting the main inputs (ebs,dos,structure,kpath,reciprocal_lattice) from the various dft parsers.
     """
-    code : str = None
-    dir : str = None
-    ebs : ElectronicBandStructure = None
-    dos : DensityOfStates = None
-    structure : Structure = None
 
-    def __init__(self,code:str , dir : str):
+    code: str = None
+    dir: str = None
+    ebs: ElectronicBandStructure = None
+    dos: DensityOfStates = None
+    structure: Structure = None
+
+    def __init__(self, code: str, dir: str):
         self.code = code
         self.dir = dir
 
@@ -41,7 +40,7 @@ class Parser:
 
         elif self.code == "bxsf":
             self.parse_bxsf()
-            
+
         elif self.code == "qe":
             self.parse_qe()
 
@@ -56,7 +55,7 @@ class Parser:
 
         elif self.code == "dftb+":
             self.parse_dftbplus()
-            
+
         if self.ebs:
             # self.ebs.bands = self.ebs.bands - self.ebs.efermi
             self.ebs.bands += self.ebs.efermi
@@ -77,24 +76,23 @@ class Parser:
         abinit_output = abinit.Output(abinit_output=outfile)
         abinit_kpoints = abinit.AbinitKpoints(filename=kpointsfile)
 
-        parser =  abinit.AbinitProcar(  
-                                        dirname=self.dir,
-                                        abinit_output=outfile,
-                                        kpath=abinit_kpoints.kpath,
-                                        reciprocal_lattice=abinit_output.reclat,
-                                        efermi=abinit_output.fermi
-                                        )
-        
+        parser = abinit.AbinitProcar(
+            dirname=self.dir,
+            abinit_output=outfile,
+            kpath=abinit_kpoints.kpath,
+            reciprocal_lattice=abinit_output.reclat,
+            efermi=abinit_output.fermi,
+        )
+
         abinit_dos = abinit.AbinitDOSParser(dirname=self.dir)
 
         self.dos = abinit_dos.dos
         self.ebs = parser.abinitprocarobject.ebs
         self.kpath = parser.abinitprocarobject.ebs.kpath
         self.structure = abinit_output.structure
-        
 
         return None
-    
+
     def parse_bxsf(self):
         """parses bxsf files.
 
@@ -103,9 +101,8 @@ class Parser:
         None
             None
         """
-        
-        parser = bxsf.BxsfParser(infile = 'in.frmsf')
 
+        parser = bxsf.BxsfParser(infile="in.frmsf")
 
         self.ebs = parser.ebs
         self.kpath = parser.kpath
@@ -113,7 +110,7 @@ class Parser:
         self.dos = parser.dos
 
         return None
-    
+
     def parse_elk(self):
         """parses bxsf files.
 
@@ -129,20 +126,19 @@ class Parser:
         #     self.dos = None
 
         try:
-            parser=elk.ElkParser(path=self.dir)
+            parser = elk.ElkParser(path=self.dir)
             self.dos = parser.dos
             self.structure = parser.structure
-            
+
         except Exception as e:
 
             self.dos = None
             self.structure = None
 
-        
         if not self.dos:
 
             try:
-                parser=elk.ElkParser(path=self.dir)
+                parser = elk.ElkParser(path=self.dir)
                 self.ebs = parser.ebs
                 self.kpath = parser.kpath
                 self.structure = parser.structure
@@ -151,7 +147,7 @@ class Parser:
                 self.kpath = None
                 self.structure = None
         return None
-    
+
     def parse_frmsf(self):
         """parses frmsf files. Needs to be finished
 
@@ -160,7 +156,7 @@ class Parser:
         None
             None
         """
-        parser = frmsf.FrmsfParser(infile = 'in.frmsf')
+        parser = frmsf.FrmsfParser(infile="in.frmsf")
 
         self.ebs = parser.ebs
         self.kpath = parser.kpath
@@ -168,7 +164,7 @@ class Parser:
         self.dos = parser.dos
 
         return None
-    
+
     def parse_lobster(self):
         """parses lobster files
 
@@ -179,10 +175,8 @@ class Parser:
         """
         code_type = self.code.split("_")[1]
         parser = lobster.LobsterParser(
-                            dirname = self.dir, 
-                            code = code_type,
-                            dos_interpolation_factor = None 
-                            )
+            dirname=self.dir, code=code_type, dos_interpolation_factor=None
+        )
 
         self.ebs = parser.ebs
         self.structure = parser.structure
@@ -190,7 +184,7 @@ class Parser:
         self.dos = parser.dos
 
         return None
-    
+
     def parse_qe(self):
         """parses qe files
 
@@ -201,20 +195,20 @@ class Parser:
         """
 
         parser = qe.QEParser(
-                            dirname = self.dir,
-                            scf_in_filename = "scf.in", 
-                            bands_in_filename = "bands.in", 
-                            pdos_in_filename = "pdos.in", 
-                            kpdos_in_filename = "kpdos.in", 
-                            atomic_proj_xml = "atomic_proj.xml"
-                            )
+            dirname=self.dir,
+            scf_in_filename="scf.in",
+            bands_in_filename="bands.in",
+            pdos_in_filename="pdos.in",
+            kpdos_in_filename="kpdos.in",
+            atomic_proj_xml="atomic_proj.xml",
+        )
 
         self.ebs = parser.ebs
         self.kpath = parser.kpath
         self.structure = parser.structure
         self.dos = parser.dos
         return None
-    
+
     def parse_siesta(self):
         """parses siesta files. Needs to be finished
 
@@ -223,10 +217,10 @@ class Parser:
         None
             None
         """
-        
+
         parser = siesta.SiestaParser(
-                            fdf_filename = f"{self.dir}{os.sep}SIESTA.fdf",
-                            )
+            fdf_filename=f"{self.dir}{os.sep}SIESTA.fdf",
+        )
 
         self.ebs = parser.ebs
         self.kpath = parser.kpath
@@ -244,7 +238,7 @@ class Parser:
             None
         """
         logger.info(f"Parsing VASP files in {self.dir}")
-        
+
         outcar = os.path.join(self.dir, "OUTCAR")
         poscar = os.path.join(self.dir, "POSCAR")
         procar = os.path.join(self.dir, "PROCAR")
@@ -253,40 +247,32 @@ class Parser:
 
         repairhandle = UtilsProcar()
         repairhandle.ProcarRepair(procar, procar)
-        
+
         outcar = vasp.Outcar(outcar)
 
-        try:
-            poscar = vasp.Poscar(poscar,rotations = outcar.rotations)
-        except Exception as e:
-            logger.exception(f"Error parsing poscar file \n{e}")
-            poscar = vasp.Poscar(poscar,rotations = None)
+        poscar = vasp.Poscar(poscar, rotations=outcar.rotations)
 
-        try:
-            kpoints = vasp.Kpoints(kpoints)
-            self.kpath = kpoints.kpath
-        except Exception as e:
-            logger.exception(f"Error parsing kpoints file \n{e}")
-            self.kpath=None
+        kpoints = vasp.Kpoints(kpoints)
+        self.kpath = kpoints.kpath
 
         procar = vasp.Procar(
-                            filename=procar,
-                            structure=poscar.structure,
-                            reciprocal_lattice=poscar.structure.reciprocal_lattice,
-                            kpath=self.kpath,
-                            n_kx=outcar.n_kx,
-                            n_ky=outcar.n_ky,
-                            n_kz=outcar.n_kz,
-                            efermi=outcar.efermi,
-                            interpolation_factor=1
-                            )
-        
+            filename=procar,
+            structure=poscar.structure,
+            reciprocal_lattice=poscar.structure.reciprocal_lattice,
+            kpath=self.kpath,
+            n_kx=outcar.n_kx,
+            n_ky=outcar.n_ky,
+            n_kz=outcar.n_kz,
+            efermi=outcar.efermi,
+            interpolation_factor=1,
+        )
+
         try:
-            vasprun = vasp.VaspXML(filename = vasprun)
+            vasprun = vasp.VaspXML(filename=vasprun)
         except Exception as e:
             logger.exception(f"Error parsing vasprun.xml file \n{e}")
             pass
-        
+
         self.ebs = procar.ebs
 
         self.structure = poscar.structure
@@ -301,7 +287,7 @@ class Parser:
 
     def parse_dftbplus(self):
         """parses DFTB+ files, these files do not have an array-like
-        structure, and the process is *slow* . 
+        structure, and the process is *slow* .
 
         Then, they are converted to VASP-like files and parsed by the
         standard `parse_vasp`
@@ -313,14 +299,14 @@ class Parser:
 
         """
         # This creates the vasp files, if needed
-        parser = dftbplus.DFTBParser(dirname = self.dir,
-                                     eigenvec_filename = 'eigenvec.out',
-                                     bands_filename = 'band.out',
-                                     detailed_out = 'detailed.out',
-                                     detailed_xml = 'detailed.xml'
-                                     )
-        
-        self.parse_vasp()
-        
-        return None
+        parser = dftbplus.DFTBParser(
+            dirname=self.dir,
+            eigenvec_filename="eigenvec.out",
+            bands_filename="band.out",
+            detailed_out="detailed.out",
+            detailed_xml="detailed.xml",
+        )
 
+        self.parse_vasp()
+
+        return None
