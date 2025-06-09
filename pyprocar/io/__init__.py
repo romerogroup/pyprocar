b19d958ecda1dce5988d71f9ca4a9e8de02109dd
lllangWV <lllangWV@gmail.com>
2025-06-09 01:28:15 -0400

    refactor(imports): Consolidate import statements in io module
    Updated import statements in __init__.py to consolidate and streamline
    the import structure. This change improves code organization and
    maintains consistency across the io module by grouping related imports
    together, enhancing readability.

--- pyprocar/io/__init__.py
+++ pyprocar/io/__init__.py
@@ -1,13 +1,6 @@
-from . import vasp 
-from . import qe
-from . import lobster
-from . import abinit
-from . import siesta
-from . import elk
-from . import dftbplus
+from pyprocar.io import abinit, dftbplus, elk, lobster, qe, siesta, vasp
+from pyprocar.io.parser import Parser
+
 # from . import bsxf
 # from . import frmsf
-from .procarparser import ProcarParser
-
-from .parser import Parser
-
+from pyprocar.io.procarparser import ProcarParser
