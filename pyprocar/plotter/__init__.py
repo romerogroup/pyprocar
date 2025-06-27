f4817979064f85ad02da82db4fd42d83ad6b5b74
lllangWV <lllangWV@gmail.com>
2025-06-27 09:24:57 -0400

    feat(plotter): Add FermiPlotter and FermiSlicePlotter imports
    This commit introduces imports for the FermiPlotter and
    FermiSlicePlotter classes in the __init__.py file. The addition
    aims to enhance the plotting capabilities related to Fermi surfaces
    by making these classes readily available for use in the plotter
    module.

--- pyprocar/plotter/__init__.py
+++ pyprocar/plotter/__init__.py
@@ -6,4 +6,5 @@ from pyprocar.plotter.bs_2d_plot import (
 from pyprocar.plotter.dos_plot import DOSPlot
 from pyprocar.plotter.ebs_plot import EBSPlot
 from pyprocar.plotter.fermi3d_plot import FermiDataHandler, FermiVisualizer
+from pyprocar.plotter.fs_plot import FermiPlotter, FermiSlicePlotter
 from pyprocar.plotter.procarplot import ProcarPlot
