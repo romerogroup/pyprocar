385b57b890cf1d5e3b3bd3d1f929d9d9584d5c0e
lllangWV <lllangWV@gmail.com>
2025-07-01 13:05:54 -0400

    feat(imports): Update imports in core and plotter modules
    This commit modifies the import statements in the core and plotter
    modules to include additional classes and functions related to
    ElectronicBandStructure and band structure visualization. The changes
    aim to enhance the modularity and accessibility of these components
    for future development and usage.
    
    The new imports include ElectronicBandStructureMesh,
    ElectronicBandStructurePath, ElectronicBandStructurePlane,
    get_ebs_from_code, and get_ebs_from_data in the core module,
    as well as BandStructure2DPlotter and BandStructurePlotter in
    the plotter module.

--- pyprocar/core/__init__.py
+++ pyprocar/core/__init__.py
@@ -2,7 +2,14 @@
 from pyprocar.core.bandstructure2D import BandStructure2D
 from pyprocar.core.brillouin_zone import BrillouinZone, BrillouinZone2D
 from pyprocar.core.dos import DensityOfStates
-from pyprocar.core.ebs import ElectronicBandStructure
+from pyprocar.core.ebs import (
+    ElectronicBandStructure,
+    ElectronicBandStructureMesh,
+    ElectronicBandStructurePath,
+    ElectronicBandStructurePlane,
+    get_ebs_from_code,
+    get_ebs_from_data,
+)
 from pyprocar.core.fermisurface import FermiSurface
 from pyprocar.core.fermisurface2D import FermiSurface2D
 from pyprocar.core.fermisurface3D import FermiSurface3D
--- pyprocar/plotter/__init__.py
+++ pyprocar/plotter/__init__.py
@@ -3,6 +3,7 @@ from pyprocar.plotter.bs_2d_plot import (
     BandStructure2DataHandler,
     BandStructure2DVisualizer,
 )
+from pyprocar.plotter.bs_plot import BandStructure2DPlotter, BandStructurePlotter
 from pyprocar.plotter.dos_plot import DOSPlot
 from pyprocar.plotter.ebs_plot import EBSPlot
 from pyprocar.plotter.fermi3d_plot import FermiDataHandler, FermiVisualizer
