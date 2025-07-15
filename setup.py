f4d892b59bef5553417f65aed1ca3b38555c2639
lllangWV <lllangWV@gmail.com>
2025-07-15 14:18:38 -0400

    chore(dependencies): Add build dependencies to pyproject.toml
    This commit updates the pyproject.toml file to include 'setuptools-scm',
    'setuptools', and 'build' in the build dependencies section. This change
    ensures that the necessary tools are available for building the package,
    improving the reliability of the build process.
    
    Additionally, the setup.py file is modified to maintain formatting, and
    the GitHub Actions workflow is updated to remove redundant installation
    of build and setuptools-scm, streamlining the installation process.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 from setuptools_scm import ScmVersion
 
+
 def version_for_project(version: ScmVersion) -> str:
    return str(version.tag)
 
-setup(use_scm_version={'version_scheme':version_for_project},
+setup(
     package_data={"": ["setup.json", '*.ini','*.yml']},
     scripts=["scripts/procar.py"],
     )
