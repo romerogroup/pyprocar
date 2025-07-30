550fca856e60063390032439b1748ca4a13e6835
lllangWV <lllangWV@gmail.com>
2025-07-30 14:04:54 -0400

    feat(splash): Replace print statements with logging for user messages
    This commit updates the welcome function in splash.py to utilize
    the logging module instead of print statements. This change aims
    to improve the logging capabilities of the application by allowing
    for better control over message levels and outputs, enhancing
    the overall user experience during application startup.

--- pyprocar/utils/splash.py
+++ pyprocar/utils/splash.py
@@ -1,19 +1,22 @@
+import logging
+
 import pyprocar
 
+user_logger = logging.getLogger("user")
 
 def welcome():
-    print(
+    user_logger.info(
         " ____        ____\n|  _ \ _   _|  _ \ _ __ ___   ___ __ _ _ __ \n| |_) | | | | |_) | '__/ _ \ / __/ _` | '__|\n|  __/| |_| |  __/| | | (_) | (_| (_| | |   \n|_|    \__, |_|   |_|  \___/ \___\__,_|_|\n       |___/"
     )
-    print("A Python library for electronic structure pre/post-processing.\n")
-    print("Version %s created on %s\n" % (pyprocar.__version__, pyprocar.__date__))
-    print(
+    user_logger.info("A Python library for electronic structure pre/post-processing.\n")
+    user_logger.info("Version %s created on %s\n" % (pyprocar.__version__, pyprocar.__date__))
+    user_logger.info(
         "Please cite:\n\
 - Uthpala Herath, Pedram Tavadze, Xu He, Eric Bousquet, Sobhit Singh, Francisco Muñoz and Aldo Romero.,\n \
  PyProcar: A Python library for electronic structure pre/post-processing.,\n \
  Computer Physics Communications 251, 107080 (2020).\n"
     )
-    print(
+    user_logger.info(
         "\
 - L. Lang, P. Tavadze, A. Tellez, E. Bousquet, H. Xu, F. Muñoz, N. Vasquez, U. Herath, and A. H. Romero,\n \
  Expanding PyProcar for new features, maintainability, and reliability.,\n \
@@ -33,6 +36,6 @@ Developers:
 - Logan Lang
 - Freddy Farah
     """
-    print(dev_string)
+    user_logger.info(dev_string)
 
     return
