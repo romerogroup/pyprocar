92414f658e276cab961387c0aa8dc1c4e6e35482
lllangWV <lllangWV@gmail.com>
2025-08-12 07:58:06 -0400

    refactor(qe): Streamline QEParser and enhance kpoints handling
    Refactored the QEParser class to remove unnecessary attributes and
    methods, consolidating the logic for handling kpoints and bands.
    This change improves code clarity and maintainability by utilizing
    cached properties for efficient data retrieval.
    
    - Removed redundant parameters and properties from the constructor
    - Introduced new methods for kpoints and bands retrieval
    - Enhanced error handling for missing data sources
    - Updated the structure method to directly use cached properties

--- pyprocar/io/qe/core.py
+++ pyprocar/io/qe/core.py
@@ -26,90 +26,7 @@ from pyprocar.utils.units import AU_TO_ANG, HARTREE_TO_EV
 logger = logging.getLogger(__name__)
 user_logger = logging.getLogger("user")
 
-
-
 class QEParser:
-    """The class is used to parse Quantum Expresso files.
-    The most important objects that comes from this parser are the .ebs and .dos
-
-    Parameters
-    ----------
-    dirpath : str, optional
-        Directory path to where calculation took place, by default ""
-    scf_in_filepath : str, optional
-        The scf filename, by default "scf.in"
-    bands_in_filepath : str, optional
-        The bands filename in the case of a band structure calculation, by default "bands.in"
-    pdos_in_filepath : str, optional
-        The pdos filename in the case of a density ofstates calculation, by default "pdos.in"
-    kpdos_in_filepath : str, optional
-        The kpdos filename, by default "kpdos.in"
-    atomic_proj_xml : str, optional
-        The atomic projection xml name. This is located in the where the outdir is and in the {prefix}.save directory, by default "atomic_proj.xml"
-    """
-
-    def __init__(
-        self,
-        dirpath: Union[str, Path] = "",
-        scf_in_filepath: Union[str, Path] = "scf.in",
-        scf_out_filepath: Union[str, Path] = "scf.out",
-        bands_in_filepath: Union[str, Path] = "bands.in",
-        pdos_in_filepath: Union[str, Path] = "pdos.in",
-        pdos_out_filepath: Union[str, Path] = "pdos.out",
-        kpdos_in_filepath: Union[str, Path] = "kpdos.in",
-        kpdos_out_filepath: Union[str, Path] = "kpdos.out",
-        data_xml_filepath: Union[str, Path] = "data-file-schema.xml",
-        atomic_proj_xml_filepath: Union[str, Path] = "atomic_proj.xml",
-    ) -> None:
-        self._scf_in_filepath = Path(scf_in_filepath)
-        self._scf_out_filepath = Path(scf_out_filepath)
-        self._bands_in_filepath = Path(bands_in_filepath)
-        self._pdos_in_filepath = Path(pdos_in_filepath)
-        self._kpdos_in_filepath = Path(kpdos_in_filepath)
-        self._atomic_proj_xml_filepath = Path(atomic_proj_xml_filepath)
-        self._pdos_out_filepath = Path(pdos_out_filepath)
-        self._kpdos_out_filepath = Path(kpdos_out_filepath)
-        self._data_xml_filepath = Path(data_xml_filepath)
-
-    @property
-    def scf_in_filepath(self) -> Path:
-        return self._scf_in_filepath
-    
-    @property
-    def scf_out_filepath(self) -> Path:
-        return self._scf_out_filepath
-
-    @property
-    def bands_in_filepath(self) -> Path:
-        return self._bands_in_filepath
-
-    @property
-    def pdos_in_filepath(self) -> Path:
-        return self._pdos_in_filepath
-
-    @property
-    def pdos_out_filepath(self) -> Path:
-        return self._pdos_out_filepath
-
-    @property
-    def kpdos_in_filepath(self) -> Path:
-        return self._kpdos_in_filepath
-
-    @property
-    def kpdos_out_filepath(self) -> Path:
-        return self._kpdos_out_filepath
-
-    @property
-    def atomic_proj_xml_filepath(self) -> Path:
-        return self._atomic_proj_xml_filepath
-    
-    @cached_property
-    def scf_in(self) -> PwIn:
-        return PwIn(self.scf_in_filepath)
-    
-    
-    
-class QEParserAuto:
     """Auto-detects Quantum ESPRESSO files in a directory and exposes
     lazy parser properties and computed objects (EBS, DOS, Structure).
 
@@ -364,97 +281,262 @@ class QEParserAuto:
             return PwXML(fp)
         except Exception:
             return None
+        
+    @cached_property
+    def kpath(self) -> Optional[KPath]:
+        if self.bands_in is None:
+            return None
+        return KPath(
+            knames=self.bands_in.kpoints_card.modified_knames,
+            special_kpoints=self.bands_in.kpoints_card.special_kpoints,
+            kticks=self.bands_in.kpoints_card.kticks,
+            ngrids=self.bands_in.kpoints_card.ngrids,
+            has_time_reversal=True,
+        )
+        
+    @cached_property
+    def kpoints(self) -> Optional[np.ndarray]:
+        kpoints = None
+        if self.atomic_proj_xml is not None:
+            kpoints = self.atomic_proj_xml.kpoints
+        elif self.projwfc_out is not None and self.projwfc_out.kpoints is not None:
+            kpoints = self.projwfc_out.kpoints
+        elif self.bands_in is not None and self.bands_in.kpoints_card is not None and self.bands_in.kpoints_card.kpoints is not None:
+            kpoints = self.bands_in.kpoints_card.kpoints
+        elif self.pw_xml is not None and self.pw_xml.kpoints is not None:
+            kpoints = self.pw_xml.kpoints
+        else:
+            user_logger.warning("No kpoints found in atomic_proj.xml or projwfc.out or bands.in")
+            return None
+        return kpoints
+    
+    @cached_property
+    def nk1(self) -> Optional[int]:
+        if self.nscf_in is not None and self.nscf_in.kpoints_card is not None and self.nscf_in.kpoints_card.nk1 is not None:
+            return self.nscf_in.kpoints_card.nk1
+        if self.pw_xml is not None and self.pw_xml.nk1 is not None:
+            return self.pw_xml.nk1
+        return None
+    
+    @cached_property
+    def nk2(self) -> Optional[int]:
+        if self.nscf_in is not None and self.nscf_in.kpoints_card is not None and self.nscf_in.kpoints_card.nk2 is not None:
+            return self.nscf_in.kpoints_card.nk2
+        if self.pw_xml is not None and self.pw_xml.nk2 is not None:
+            return self.pw_xml.nk2
+        return None
+    
+    @cached_property
+    def nk3(self) -> Optional[int]:
+        if self.nscf_in is not None and self.nscf_in.kpoints_card is not None and self.nscf_in.kpoints_card.nk3 is not None:
+            return self.nscf_in.kpoints_card.nk3
+        if self.pw_xml is not None and self.pw_xml.nk3 is not None:
+            return self.pw_xml.nk3
+        return None
+    
+    @cached_property
+    def sk1(self) -> Optional[int]:
+        if self.nscf_in is not None and self.nscf_in.kpoints_card is not None and self.nscf_in.kpoints_card.sk1 is not None:
+            return self.nscf_in.kpoints_card.sk1
+        if self.pw_xml is not None and self.pw_xml.sk1 is not None:
+            return self.pw_xml.sk1
+        return None
+    
+    @cached_property
+    def sk2(self) -> Optional[int]:
+        if self.nscf_in is not None and self.nscf_in.kpoints_card is not None and self.nscf_in.kpoints_card.sk2 is not None:
+            return self.nscf_in.kpoints_card.sk2
+        if self.pw_xml is not None and self.pw_xml.sk2 is not None:
+            return self.pw_xml.sk2
+        return None
+    
+    @cached_property
+    def sk3(self) -> Optional[int]:
+        if self.nscf_in is not None and self.nscf_in.kpoints_card is not None and self.nscf_in.kpoints_card.sk3 is not None:
+            return self.nscf_in.kpoints_card.sk3
+        if self.pw_xml is not None and self.pw_xml.sk3 is not None:
+            return self.pw_xml.sk3
+        return None
+    
+    @cached_property
+    def reciprocal_lattice(self) -> Optional[np.ndarray]:
+        if self.pw_xml is not None and self.pw_xml.reciprocal_lattice is not None:
+            return self.pw_xml.reciprocal_lattice
+        if self.scf_out is not None and self.scf_out.reciprocal_axes is not None:
+            return self.scf_out.reciprocal_axes
+        elif self.bands_out is not None and self.bands_out.reciprocal_axes is not None:
+            return self.bands_out.reciprocal_axes
+        elif self.nscf_out is not None and self.nscf_out.reciprocal_axes is not None:
+            return self.nscf_out.reciprocal_axes
+        return None
+    
+    @cached_property
+    def fermi(self) -> Optional[float]:
+        if self.scf_out is not None and self.scf_out.fermi_energy_ev is not None:
+            return self.scf_out.fermi_energy_ev
+        if self.pw_xml is not None and self.pw_xml.fermi is not None:
+            return self.pw_xml.fermi
+        return None
+    
+    @cached_property
+    def bands(self) -> Optional[np.ndarray]:
+        if self.atomic_proj_xml is not None and self.atomic_proj_xml.bands is not None:
+            return self.atomic_proj_xml.bands
+        elif self.projwfc_out is not None and self.projwfc_out.bands is not None:
+            return self.projwfc_out.bands
+        elif self.pw_xml is not None and self.pw_xml.bands is not None:
+            return self.pw_xml.bands
 
+        user_logger.warning("No bands found in atomic_proj.xml or projwfc.out or pw.xml")
+        return None
+    
+    @cached_property
+    def spd_phase(self) -> Optional[np.ndarray]:
+        if self.atomic_proj_xml is None and self.projwfc_out is None:
+            return None
+        
+        wfc_mapping = self.projwfc_out.wfc_mapping
+        projections = self.atomic_proj_xml.projections
+        orbitals = self.projwfc_out.orbitals
+        
+        n_kpoints = self.atomic_proj_xml.n_kpoints
+        n_bands = self.atomic_proj_xml.n_bands
+        n_spin_channels = self.atomic_proj_xml.n_spin_channels
+        n_atoms = self.projwfc_out.n_atoms
+        n_orbitals = self.projwfc_out.n_orbitals
+
+        
+        pyprocar_projections_phase = np.zeros(
+            shape=(n_kpoints, n_bands, n_spin_channels, n_atoms, n_orbitals),
+            dtype=projections.dtype,
+        )
+        
+        for state_num, wfc_info in wfc_mapping.items():
+            element = wfc_info["element"]
+            wfc_num = wfc_info["wfc_num"]
+            atm_num = wfc_info["atm_num"]
+            l = wfc_info["l"]
+            j = wfc_info["j"]
+            m_j = wfc_info["m_j"]
+            m = wfc_info["m"]
+            
+            if m_j is not None:
+                orbital_dict = {
+                    "l": l,
+                    "j": j,
+                    "m_j": m_j,
+                }
+            else:
+                orbital_dict = {
+                    "l": l,
+                    "m": m,
+                }
+            
+            i_orbital = orbitals.index(orbital_dict)
+            i_atom = atm_num - 1
+            i_state = state_num - 1
+            pyprocar_projections_phase[..., i_atom, i_orbital] += projections[..., i_state]
+            
+        return pyprocar_projections_phase
+    
+    @cached_property
+    def spd(self) -> Optional[np.ndarray]:
+        if self.atomic_proj_xml is None and self.projwfc_out is None and self.spd_phase is None:
+            return None
+        
+        return self.spd_phase.real
+    
     # -------- computed properties --------
     @cached_property
     def ebs(self) -> Optional[ElectronicBandStructure]:
-        # Fermi energy
-        efermi = None
-        if self.scf_out and self.scf_out.fermi_energy_ev is not None:
-            efermi = self.scf_out.fermi_energy_ev
-        elif self.nscf_out and self.nscf_out.fermi_energy_ev is not None:
-            efermi = self.nscf_out.fermi_energy_ev
-        if efermi is None:
-            user_logger.warning("Fermi energy not found in SCF/NSCF outputs")
-            return None
-
-        # kpoints and bands
-        if self.atomic_proj_xml and self.atomic_proj_xml.kpoints is not None and self.atomic_proj_xml.bands is not None:
-            kpoints = self.atomic_proj_xml.kpoints
-            bands = self.atomic_proj_xml.bands
-            weights = self.atomic_proj_xml.weights
-            logger.info("EBS: using atomic_proj.xml for kpoints/bands")
-        elif self.projwfc_out and self.projwfc_out.kpoints is not None and self.projwfc_out.bands is not None:
-            kpoints = self.projwfc_out.kpoints
-            bands = self.projwfc_out.bands
-            weights = None
-            logger.info("EBS: using projwfc.out for kpoints/bands")
-        else:
-            user_logger.warning("No bands source found (atomic_proj.xml or projwfc.out)")
-            return None
-
-        reciprocal_lattice = None
-        if self.pw_xml and self.pw_xml.reciprocal_lattice is not None:
-            reciprocal_lattice = self.pw_xml.reciprocal_lattice
-        elif self.scf_out and self.scf_out.reciprocal_axes is not None:
-            reciprocal_lattice = self.scf_out.reciprocal_axes
 
         return ElectronicBandStructure(
-            kpoints=kpoints,
-            bands=bands,
-            efermi=efermi,
-            projected=None,
-            projected_phase=None,
-            weights=weights,
-            kpath=None,
-            labels=None,
-            reciprocal_lattice=reciprocal_lattice,
-        )
+                kpoints=self.kpoints,
+                n_kx=self.nk1,
+                n_ky=self.nk2,
+                n_kz=self.nk3,
+                bands=self.bands,
+                projected=self.spd,
+                efermi=self.fermi,
+                kpath=self.kpath,
+                projected_phase=self.spd_phase,
+                labels=self.projwfc_out.orbitals,
+                reciprocal_lattice=self.reciprocal_lattice,
+            )
+        
+    @cached_property
+    def projected_dos(self) -> Optional[np.ndarray]:
+        if self.projwfc_dos is None:
+            return None
+        return self.projwfc_dos.projected_dos
+    
+    @cached_property
+    def total_dos(self) -> Optional[np.ndarray]:
+        if self.projwfc_dos is None:
+            return None
+        return self.projwfc_dos.total_dos
+    
+    @cached_property
+    def energies(self) -> Optional[np.ndarray]:
+        if self.projwfc_dos is None:
+            return None
+        return self.projwfc_dos.bands[0, :]
 
     @cached_property
     def dos(self) -> Optional[DensityOfStates]:
         if self.projwfc_dos is None:
             user_logger.warning("No PDOS files found for DOS construction")
             return None
+        
         try:
-            energies_grid = self.projwfc_dos.energies
-            energies = energies_grid[0, :]
-            total_dos = self.projwfc_dos.total_dos
-            if total_dos is None:
-                user_logger.warning("Total DOS not found in PDOS files")
-                return None
-            total = total_dos.T  # (n_spin, n_energies)
-            efermi = self.scf_out.fermi_energy_ev if self.scf_out else 0.0
-            return DensityOfStates(energies=energies, total=total, efermi=efermi)
+            return DensityOfStates(
+                    energies=self.energies,
+                    total=self.total_dos,
+                    efermi=self.fermi,
+                    projected=self.projected_dos,
+                )
         except Exception as exc:
             user_logger.warning(f"Error building DOS: {exc}")
+            return None
+        
+    @cached_property
+    def species(self) -> Optional[List[str]]:
+        if self.pw_xml is not None and self.pw_xml.atomic_species is not None:
+            return self.pw_xml.atomic_species
+        else:
+            user_logger.warning("No atomic species found in any input or output file")
+            return None
+        
+    @cached_property
+    def direct_lattice(self) -> Optional[np.ndarray]:
+        if self.pw_xml is not None and self.pw_xml.direct_lattice is not None:
+            return self.pw_xml.direct_lattice
+        else:
+            user_logger.warning("No direct lattice found in any input or output file")
+            return None
+        
+    @cached_property
+    def atomic_positions(self) -> Optional[np.ndarray]:
+        if self.pw_xml is not None and self.pw_xml.atomic_positions is not None:
+            return self.pw_xml.atomic_positions
+        else:
+            user_logger.warning("No atomic positions found in any input or output file")
+            return None
+        
+    @cached_property
+    def rotations(self) -> Optional[np.ndarray]:
+        if self.pw_xml is not None and self.pw_xml.rotations is not None:
+            return self.pw_xml.rotations
+        else:
+            user_logger.warning("No rotations found in any input or output file")
             return None
 
     @cached_property
     def structure(self) -> Optional[Structure]:
-        if self.pw_xml and self.pw_xml.direct_lattice is not None and self.pw_xml.atomic_species is not None and self.pw_xml.atomic_positions is not None:
-            try:
-                lattice = self.pw_xml.direct_lattice
-                atoms = list(self.pw_xml.atomic_species)
-                frac = self.pw_xml.atomic_positions
-                logger.info("Structure: using PwXML")
-                return Structure(atoms=atoms, fractional_coordinates=frac, lattice=lattice)
-            except Exception as exc:
-                user_logger.warning(f"Failed to build Structure from PwXML: {exc}")
+        return Structure(
+            atoms=self.species,
+            lattice=self.direct_lattice,
+            fractional_coordinates=self.atomic_positions,
+            rotations=self.rotations,
+        )
 
-        if self.scf_out and self.scf_out.crystal_axes is not None and self.scf_out.atomic_sites is not None and self.scf_out.alat is not None:
-            try:
-                lattice = np.array(self.scf_out.crystal_axes, dtype=float) * float(self.scf_out.alat) * AU_TO_ANG
-                atoms = [site["symbol"] for site in self.scf_out.atomic_sites]
-                frac = np.array([[site["x"], site["y"], site["z"]] for site in self.scf_out.atomic_sites], dtype=float)
-                logger.info("Structure: using PwOut")
-                return Structure(atoms=atoms, fractional_coordinates=frac, lattice=lattice)
-            except Exception as exc:
-                user_logger.warning(f"Failed to build Structure from PwOut: {exc}")
-
-        user_logger.warning("Unable to build Structure from available files")
-        return None
-
-# Backward-compatible alias
-QEParser = QEParserAuto
-    
