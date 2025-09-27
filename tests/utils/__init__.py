35038ac0800f922f2c0dd4c645818fab477739d0
Logan Lang <lllangWV@gmail.com>
2025-09-27 15:08:32 -0400

    feat(logging): Add logging setup in utils for improved debugging
    Introduced a logger in the utils module to facilitate debugging and provide detailed runtime information. Set the logger level to DEBUG, ensuring comprehensive logging of events during test execution. This enhancement aligns with the project's logging conventions and improves overall traceability in the testing environment.

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,6 +1,10 @@
 from pathlib import Path
-
+import logging
 from tests.utils.base_test import BaseTest
+
+logger = logging.getLogger("pyprocar")
+logger.setLevel(logging.DEBUG)
+
 
 ROOT_DIR = Path(__file__).parent.parent.parent
 TEST_DIR = ROOT_DIR / "tests"
