3e32a565d6661036bf407fe387a59c2a6ba8924d
Logan Lang <lllangWV@gmail.com>
2026-01-03 17:53:18 -0500

    chore(pixi.toml, pyproject.toml, setup.py): Update dependencies and clean up configuration files
    Added 'editables' and 'pandas-stubs' dependencies in pixi.toml, adjusted Python version constraint, and removed unused configuration sections from pyproject.toml. Also, standardized package data formatting in setup.py for consistency. These changes enhance dependency management and improve overall project configuration.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(
-    package_data={"": ["setup.json", '*.ini','*.yml']},
+    package_data={"": ["setup.json", "*.ini", "*.yml"]},
     scripts=["scripts/procar.py"],
-    )
+)
